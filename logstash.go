6b97647ba58f524171bf6eb41ac2a65b77613c45
root <root@mesos-slave-03.prod.gravity-dev.gravity.gsenext.com>
2016-02-04 10:20:54 +0900

    Removed unneccessary logs

--- logstash.go
+++ logstash.go
@@ -51,10 +51,10 @@ func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
 		
 		for _, kv := range m.Container.Config.Env {
 			kvp := strings.SplitN(kv, "=", 2)
-			log.Println("[LOGSTASH ADAPTOR]: Key = ", kvp[0])
-			log.Println("[LOGSTASH ADAPTOR]: Value = ", kvp[1])
+			//log.Println("[LOGSTASH ADAPTOR]: Key = ", kvp[0])
+			//log.Println("[LOGSTASH ADAPTOR]: Value = ", kvp[1])
 			if kvp[0] == "LOGID" {
-				log.Println("Setting the LogId to the value to the container")			
+				//log.Println("Setting the LogId to the value to the container")			
 				msg.LogId = kvp[1] 
 			} 
 		}
